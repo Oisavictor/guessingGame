321cc9765e06d289da8b6097f63b65e11f6c0db2
Victor Oisamoje <Oisavictor@gmail.com>
2022-06-02 14:35:57 +0000

    Added total Points

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // initializing variables
-x = 2;
+x = 1;
 points = 0;
 
 // Welcome the user to make a guess
@@ -7,16 +7,16 @@ console.log("Hi dear, are you up for a game!....\nGuess a number that could make
 
 //The logic
 while(x) {
-  num = Math.round(Math.random()*x) + 1;
+  num = Math.round(Math.random()* x) + 1;
   console.log('\n');
-  console.log(`Enter a number between 1 - ${x} | To exist Enter the number 0`);
+  console.log(`Enter a number between 1 - ${x+1} | To exist Enter the number 0`);
   guess = prompt("Enter the number you want to guess here: ");
   
   if (guess == 0){
     break;
   }else if (guess == num){
     x++;
-    console.log(`Correct Guess!!. You have Earned: ${points = 1} Point. You are now in Stage: `,  x-1);
+    console.log(`Correct Guess!!. You have Earned: ${points = 1} Point.\nYour total point is ${x-1}.\nYou are now in Stage: `,  x-1);
   }else{
     console.log("wrong Guess. Please try again");
   }
